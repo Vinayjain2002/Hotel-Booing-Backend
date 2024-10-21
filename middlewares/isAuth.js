66d611dd662f32dc45570493b0949f9d305e0643
Vinay Jain <vinayjain61787@gmail.com>
2024-10-21 11:28:44 +0530

    Hotel Backend Ready

---
+++ middlewares/isAuth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken')
+
+// Testing acess token and returning email if valid
+module.exports = (req,res, next) => {
+    const authHeader = req.get('Authorization')
+    if(!authHeader){
+        req.isAuth = false;
+        return next()
+    }
+
+    let token = authHeader.split(' ')[1]
+    if(!token || token === ''){
+        req.isAuth = false
+        return next()
+    }
+
+    let decodeToken;
+
+    try{
+        decodeToken = jwt.verify(token, 'accessToken')
+    }
+    catch(err){
+        req.isAuth = false;
+        return next()
+    }
+
+    req.isAuth = true
+    req.email = decodeToken.email
+    next()
+}
